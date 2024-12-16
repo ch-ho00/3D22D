7073412e7a1f7502e02e1004ce6d5c5c56dd4a0b
chp <psbhchan@gmail.com>
2024-12-16 07:06:23 +0000

    cog.yaml initial version

--- inference-flux.py
+++ inference-flux.py
@@ -1,18 +1,11 @@
+# Tested on 48 GB VRAM RTX A6000
 # reference to https://github.com/black-forest-labs/flux?tab=readme-ov-file#diffusers-integration
 import torch
 from diffusers import FluxPipeline
 
 # Choose mode
-model_id = input(
-    "Choose mode: 0(black-forest-labs/FLUX.1-dev. You are supposed to have done the third step of 'https://github.com/monk-after-90s/ai-toolkit?tab=readme-ov-file#flux1-dev') \nor\n 1(black-forest-labs/FLUX.1-schnell)").strip()
-if model_id == "0":
-    model_id = "black-forest-labs/FLUX.1-dev"
-elif model_id == "1":
-    model_id = "black-forest-labs/FLUX.1-schnell"
-else:
-    raise ValueError("Invalid model id")
-
-pipe = FluxPipeline.from_pretrained(model_id, torch_dtype=torch.bfloat16)
+model_id = "black-forest-labs/FLUX.1-dev"
+pipe = FluxPipeline.from_pretrained(model_id, torch_dtype=torch.bfloat16, device_map='balanced')
 lora_dir = input("""Enter either:
                     - A string, the *model id* (for example `google/ddpm-celebahq-256`) of a pretrained model hosted on
                       the Hub.
@@ -21,28 +14,30 @@ lora_dir = input("""Enter either:
                     - A [torch state
                       dict](https://pytorch.org/tutorials/beginner/saving_loading_models.html#what-is-a-state-dict)""").strip()
 weight_name = input("Weight file with the extension name 'safetensors':").strip() or None
-
+print(f"Loading lora weights from {lora_dir} with weight name {weight_name}")
 pipe.load_lora_weights(lora_dir,
-                       weight_name=weight_name)
-pipe.enable_model_cpu_offload()  # save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
+                       weight_name=weight_name,
+                       adapter_name='lora')
+pipe.set_adapters('lora')
+pipe.fuse_lora(adapter_names=['lora'])
+# pipe.enable_model_cpu_offload()
 
 prompt = "undefined"
-seed = "undefined"
-num_inference_steps = "undefined"
-output_file = "undefined"
+seed = 42
+num_inference_steps = 28
+output_file = "test.png"
 while True:
     prompt = input(f"Prompt(default: {prompt}):").strip() or prompt
-    seed = int(input(f"Random seed(default: {seed})(int):").strip() or seed)
-    num_inference_steps = int(input(
-        f"Number of inference steps(default: {num_inference_steps})(int, 28 is recommend):").strip() or num_inference_steps)
-    output_file = input(f"Output file name(default: {output_file}):").strip() or output_file
     print(
         f"Prompt: {prompt}\nRandom seed: {seed}\nNumber of inference steps: {num_inference_steps}\nOutput file name: {output_file}")
     image = pipe(
         prompt,
         output_type="pil",
         num_inference_steps=num_inference_steps,
-        generator=torch.Generator("cpu").manual_seed(seed)
+        generator=torch.Generator("cpu").manual_seed(seed),
+        height=2048,
+        width=2048,        
     ).images[0]
+    output_file = prompt.replace(" ", "_") + ".png"
     image.save(output_file)
     print(f"Image saved to {output_file}")
--- predict.py
+++ predict.py
@@ -2,21 +2,46 @@
 # https://cog.run/python
 
 from cog import BasePredictor, Input, Path
+import torch
+from diffusers import FluxPipeline
+from PIL import Image
 
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
-        # self.model = torch.load("./weights.pth")
+        model_id = "black-forest-labs/FLUX.1-dev"
+        self.pipe = FluxPipeline.from_pretrained(model_id, torch_dtype=torch.bfloat16, device_map='balanced')
+        
+        lora_dir = "/root/RMTWTH_64rank_2res_4ksteps_lr4e-4_batch3"
+        weight_name = "RMTWTH_64rank_2res_4ksteps_lr4e-4_batch3_000002500.safetensors"
+        self.pipe.load_lora_weights(lora_dir, weight_name=weight_name, adapter_name='lora')
+        self.pipe.set_adapters('lora')
+        self.pipe.fuse_lora(adapter_names=['lora'])
 
     def predict(
         self,
-        image: Path = Input(description="Grayscale input image"),
-        scale: float = Input(
-            description="Factor to scale image by", ge=0, le=10, default=1.5
-        ),
+        prompt: str = Input(description="Text prompt for image generation"),
+        seed: int = Input(description="Random seed for reproducibility", default=42),
+        num_inference_steps: int = Input(description="Number of inference steps", default=28),
+        height: int = Input(description="Height of the generated image", default=2048),
+        width: int = Input(description="Width of the generated image", default=2048),
     ) -> Path:
         """Run a single prediction on the model"""
-        # processed_input = preprocess(image)
-        # output = self.model(processed_image, scale)
-        # return postprocess(output)
+        print(f"Prompt: {prompt}\nRandom seed: {seed}\nNumber of inference steps: {num_inference_steps}")
+        
+        image = self.pipe(
+            prompt,
+            output_type="pil",
+            num_inference_steps=num_inference_steps,
+            generator=torch.Generator("cpu").manual_seed(seed),
+            height=height,
+            width=width,
+        ).images[0]
+        
+        output_file = f"{prompt.replace(' ', '_')}.png"
+        image.save(output_file)
+        print(f"Image saved to {output_file}")
+        
+        return Path(output_file)
+
